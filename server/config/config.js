037cc466f64bc993251ebbc6083495f0dda8bce0
nikunjAg <aggarwalnikunj1999@gmail.com>
2019-02-25 18:14:26 +0530

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,20 @@
 var env = process.env.NODE_ENV || 'development';
 
-if(env == 'development') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoDB';
-} else if (env == 'test') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoDBTest';
+if ( env === 'development' || env === 'test') {
+    // automatically converts the JSON file into a javascript object
+    var config = require('./config.json');
+    // config is object if the value we want to access is variable then we need to use bracket notation
+    var envConfig = config[env];
+    // Converts all the keys in envConfig and convert them into an array
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    });
 }
+
+// if(env === 'development') {
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoDB';
+// } else if (env === 'test') {
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoDBTest';
+// }
--- server/schema/userSchema.js
+++ server/schema/userSchema.js
@@ -57,7 +57,7 @@ userSchema.methods.toJSON = function () {
 userSchema.methods.generateAuthToken = function () {
     var user = this;
     var access = 'auth';
-    var token = jwt.sign({_id: user._id.toHexString(), access}, 'someSecret').toString();
+    var token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
     user.tokens.push({access, token});
     return user.save().then(() => {
         return token;
@@ -79,7 +79,7 @@ userSchema.statics.findByToken = function(token) {
     var decode;
 
     try {
-        decode = jwt.verify(token, 'someSecret');
+        decode = jwt.verify(token, process.env.JWT_SECRET);
     } catch (e) {
         return Promise.reject();
     }
